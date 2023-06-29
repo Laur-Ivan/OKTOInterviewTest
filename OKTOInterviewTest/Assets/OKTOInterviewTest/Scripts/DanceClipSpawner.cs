da08a0c72a2abafe5653e0cdd2c07e2d02fa147a
Laur-Ivan <laurentiu.marian.ivan@gmail.com>
2023-06-29 20:55:45 +0300

    Scalable solution for spawning content slides at runtime progress

--- OKTOInterviewTest/Assets/OKTOInterviewTest/Scripts/DanceClipSpawner.cs
+++ OKTOInterviewTest/Assets/OKTOInterviewTest/Scripts/DanceClipSpawner.cs
@@ -5,28 +5,42 @@ using UnityEngine.UIElements;
 
 public class DanceClipSpawner : MonoBehaviour
 {
-    [SerializeField] private UIDocument ParentContainer; // The UIDocument that holds our whole UI
-    [SerializeField] private VisualTreeAsset DanceClip; // The UXML asset for a single DanceClip
+    [SerializeField] private UIDocument parentContainer; // The UIDocument that holds our whole UI
+    [SerializeField] private VisualTreeAsset danceClipTemplate; // The UXML asset for a single DanceClip
     [SerializeField] private int numberOfDanceClips = 3;
 
     [SerializeField] private string contentContainerName = "ContentContainer";
+    [SerializeField] private string characterRenderTextureName = "CharacterRenderTexture";
     
+    [SerializeField] private RenderTexture[] characterRenderTextures;
+
+    // Reference to your background images for the DanceClips
+    [SerializeField] private Texture2D[] backgroundImages;
+
     void Start()
     {
-        // Get the root VisualElement of the ParentContainer (our UIDocument)
-        VisualElement root = ParentContainer.rootVisualElement;
+        // Get the ContentContainer
+        ScrollView contentContainer = parentContainer.rootVisualElement.Q<ScrollView>(contentContainerName);
 
-        // Find the ScrollView where we want to spawn the dance clips. This is our ContentContainer.
-        ScrollView contentContainer = root.Q<ScrollView>(contentContainerName);
-
-        // Instantiate the dance clips
-        for (int i = 0; i < numberOfDanceClips; i++)
+        // Loop for the number of DanceClips you want to create
+        for (int i = 0; i < 3; i++)
         {
-            // Clone the DanceClip template
-            VisualElement newDanceClip = DanceClip.CloneTree();
+            // Create a new DanceClip from the template
+            VisualElement danceClip = danceClipTemplate.CloneTree().contentContainer;
 
-            // Add it to the ContentContainer
-            contentContainer.Add(newDanceClip);
+            // Get the CharacterRenderTexture VisualElement in the DanceClip
+            VisualElement characterContainer = danceClip.Q<VisualElement>(characterRenderTextureName);
+            Debug.Log($"Character container {characterContainer}");
+            
+            // Set the background image of the DanceClip to one of your textures
+            danceClip.style.backgroundImage = new StyleBackground(backgroundImages[i]);
+
+            // Set the background image of the CharacterRenderTexture to one of your RenderTextures
+            Debug.Log($"Character render texture {characterRenderTextures[i]}");
+            characterContainer.style.backgroundImage = new StyleBackground(Background.FromRenderTexture(characterRenderTextures[i]));
+
+            // Add the DanceClip to the ContentContainer
+            contentContainer.Add(danceClip);
         }
     }
 }
